208691b29780feffcd89a27474bb3459fcdc0656
Andi Miller <andi@andimiller.net>
2017-01-20 02:30:48 +0000

    check in a working example of Record types with scodecs

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "bit-packer"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+
+libraryDependencies += "org.scodec" %% "scodec-core" % "1.10.3"
+
+libraryDependencies += "org.scodec" %% "scodec-bits" % "1.1.4"
---
+++ src/main/scala/BitRecord.scala
@@ -0,0 +1,41 @@
+package scodec.codecs.bitpacker
+
+import scodec.{Codec, SizeBound}
+import scodec.codecs._
+import scodec.bits._
+import shapeless._
+import poly._
+import record._
+import shapeless.labelled.FieldType
+import shapeless.syntax.SingletonOps
+import syntax.singleton._
+
+/**
+  * Created by Andi on 20/01/2017.
+  */
+object BitRecord extends App {
+
+  object ExtraCodecs {
+    val uint3: Codec[Int] = new IntCodec(3, false, ByteOrdering.BigEndian)
+    val uint5: Codec[Int] = new IntCodec(5, false, ByteOrdering.BigEndian)
+    val uint6: Codec[Int] = new IntCodec(6, false, ByteOrdering.BigEndian)
+    val uint7: Codec[Int] = new IntCodec(7, false, ByteOrdering.BigEndian)
+    val uint9: Codec[Int] = new IntCodec(9, false, ByteOrdering.BigEndian)
+    val uint10: Codec[Int] = new IntCodec(10, false, ByteOrdering.BigEndian)
+    val uint11: Codec[Int] = new IntCodec(11, false, ByteOrdering.BigEndian)
+    val uint12: Codec[Int] = new IntCodec(12, false, ByteOrdering.BigEndian)
+
+  }
+  import ExtraCodecs._
+
+  val format = ("age" ->> uint3) :: ("hasTail" ->> bool) :: ("pin" ->> uint12) :: HNil
+
+  val result = format.values.toCodec.decode(bin"0101010101010101").map(x => x.value.zipWithKeys(format.keys))
+
+  result.map { r =>
+    println(s"the record's age is ${r("age")}")
+    println(s"the record's tail status is ${r("hasTail")}")
+    println(s"the record's pin number is ${r("pin")}")
+    //println(s"the record's missing attribute is ${r("nothing")}")
+  }
+}
